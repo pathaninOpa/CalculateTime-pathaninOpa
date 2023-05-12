8dda34ddff801022d2b04ec997757f9e10f3ef82
pathaninOpa <61925150+pathaninOpa@users.noreply.github.com>
2023-05-12 05:14:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CalculateTime-pathaninOpa/Time.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+void getTime(int &h,int &m){
+  cin>>h>>m;
+}
+
+int subtract(int h1, int m1,int h2,int m2)
+{
+  return h1;
+}
+int subtract(int m1, int m2)
+{
+  return m2-m1;
+}
